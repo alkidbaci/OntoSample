447cbceee2242d1d3fe4e75962a0023a26294e71
Alkid <alkid1baci@gmail.com>
2024-11-30 18:42:04 +0100

    version increase for owlapy, strict version for owlready2, new version for ontosample

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open('README.md', 'r') as fh:
 setup(
     name="ontosample",
     description="Ontosample is a package that offers different sampling techniques for OWL ontologies.",
-    version="0.2.5",
+    version="0.2.6",
     packages=find_packages(),
     install_requires=[
         "matplotlib>=3.3.4",
-        "owlready2>=0.40",
+        "owlready2==0.40",
         "torch>=1.7.1",
         "pandas>=1.5.0",
         "sortedcontainers>=2.4.0",
-        "owlapy==1.3.0",
+        "owlapy==1.3.3",
         "requests>=2.31.0",
         "deap>=1.3.1"],
     author='Alkid Baci',
